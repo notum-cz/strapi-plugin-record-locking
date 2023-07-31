44daf6f8c1837a649f53ef29cd306fc7ef1da835
Ondrej Mikulcik <ondrej.mikulcik@notum.cz>
2023-07-31 15:14:21 +0200

    feat: added role check when locking content

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -6,14 +6,37 @@ module.exports = ({ strapi }) => {
 
   io.on("connection", function (socket) {
     socket.on("openEntity", async ({ entityId, entitySlug, userId }) => {
-      await strapi.db.query("plugin::record-locking.open-entity").create({
-        data: {
-          user: String(userId),
-          entityType: entitySlug,
-          entityIdentifier: entityId,
-          connectionId: socket.conn.id,
-        },
-      });
+      const usersPermissionsForThisContent = await strapi.db.connection
+        .select("p.id", "p.action", "p.subject")
+        .from("admin_permissions AS p")
+        .innerJoin(
+          "admin_permissions_role_links AS prl",
+          "p.id",
+          "prl.permission_id"
+        )
+        .innerJoin(
+          "admin_users_roles_links AS url",
+          "prl.role_id",
+          "url.role_id"
+        )
+        .where("url.user_id", userId)
+        .andWhere("p.subject", entitySlug);
+      const userHasAdequatePermissions =
+        usersPermissionsForThisContent.filter((perm) =>
+          ["create", "delete", "publish"].some((operation) =>
+            perm.action.includes(operation)
+          )
+        ).length !== 0;
+      if (userHasAdequatePermissions) {
+        await strapi.db.query("plugin::record-locking.open-entity").create({
+          data: {
+            user: String(userId),
+            entityType: entitySlug,
+            entityIdentifier: entityId,
+            connectionId: socket.conn.id,
+          },
+        });
+      }
     });
 
     socket.on("closeEntity", async ({ entityId, entitySlug, userId }) => {
