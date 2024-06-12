afd8b915d10b7da9b93290c59bf4cfd9c1d71b04
Ondrej Mikulcik <ondrej.mikulcik@notum.cz>
2024-06-12 11:13:24 +0200

    fix: add optional chaining to prevent CLI errors

--- server/destroy.js
+++ server/destroy.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 
 module.exports = ({ strapi }) => {
   if (strapi?.io?.close) {
     strapi.io.close();
   }
-  strapi.db.query("plugin::record-locking.open-entity").deleteMany();
+  strapi.db?.query("plugin::record-locking.open-entity").deleteMany();
 };
