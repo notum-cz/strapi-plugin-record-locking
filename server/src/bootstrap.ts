5cb8b3aab31dd624830d1e232367e28cbf2644b0
Punit Sethi <punitsethi@gmail.com>
2025-10-01 14:51:50 +0530

    Fix: Use sessionManager to decode token -> userId Strapi v5.24.0 onwards

--- server/src/bootstrap.ts
+++ server/src/bootstrap.ts
@@ -8,30 +8,39 @@ const bootstrap = ({ strapi }: { strapi: Core.Strapi }) => {
 
   io.on('connection', (socket) => {
     socket.on('openEntity', async ({ entityDocumentId, entityId }) => {
-      const userId = strapi.admin.services.token.decodeJwtToken(socket.handshake.auth.token).payload
-        .id;
+      try {
+        // @ts-ignore
+        const userId = strapi.admin.services.token.decodeJwtToken?.(socket.handshake.auth.token)?.payload?.id ||
+          // @ts-ignore
+          strapi.sessionManager?.('admin').validateAccessToken?.(socket.handshake.auth.token)?.payload?.userId ||
+          null;
 
-      const usersPermissionsForThisContent = await strapi.db.connection
-        .select('p.id', 'p.action', 'p.subject')
-        .from('admin_permissions AS p')
-        .innerJoin('admin_permissions_role_lnk AS prl', 'p.id', 'prl.permission_id')
-        .innerJoin('admin_users_roles_lnk AS url', 'prl.role_id', 'url.role_id')
-        .where('url.user_id', userId)
-        .andWhere('p.subject', entityId);
-      const userHasAdequatePermissions =
-        usersPermissionsForThisContent.filter((perm) =>
-          ['create', 'delete', 'publish'].some((operation) => perm.action.includes(operation))
-        ).length !== 0;
-      if (userHasAdequatePermissions) {
-        await strapi.db.query('plugin::record-locking.open-entity').create({
-          data: {
-            user: String(userId),
-            entityId,
-            entityDocumentId,
-            connectionId: socket.id,
-          },
-        });
-      }
+        if (userId) {
+          const usersPermissionsForThisContent = await strapi.db.connection
+          .select('p.id', 'p.action', 'p.subject')
+          .from('admin_permissions AS p')
+          .innerJoin('admin_permissions_role_lnk AS prl', 'p.id', 'prl.permission_id')
+          .innerJoin('admin_users_roles_lnk AS url', 'prl.role_id', 'url.role_id')
+          .where('url.user_id', userId)
+          .andWhere('p.subject', entityId);
+          const userHasAdequatePermissions =
+            usersPermissionsForThisContent.filter((perm) =>
+              ['create', 'delete', 'publish'].some((operation) => perm.action.includes(operation))
+            ).length !== 0;
+          if (userHasAdequatePermissions) {
+            await strapi.db.query('plugin::record-locking.open-entity').create({
+              data: {
+                user: String(userId),
+                entityId,
+                entityDocumentId,
+                connectionId: socket.id,
+              },
+            });
+          }
+        }
+    } catch (error) {
+      console.error(error);
+    }
     });
 
     socket.on('closeEntity', async ({ entityId, entityDocumentId, userId }) => {
