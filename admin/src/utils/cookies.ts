049f5b1e48b60c708f12495062ddd1a6ce194c0e
Punit Sethi <punitsethi@gmail.com>
2025-10-03 11:27:25 +0530

    Changed getCookieValue to a more compact version

--- admin/src/utils/cookies.ts
+++ admin/src/utils/cookies.ts
@@ -5,13 +5,9 @@
  * @returns The decoded cookie value if found, otherwise null.
  */
 export const getCookieValue = (name: string): string | null => {
-    let result = null;
-    const cookieArray = document.cookie.split(';');
-    cookieArray.forEach((cookie) => {
-      const [key, value] = cookie.split('=').map((item) => item.trim());
-      if (key === name) {
-        result = decodeURIComponent(value);
-      }
-    });
-    return result;
-  };
+  const cookie = document.cookie
+    .split("; ")
+    .find((c) => c.startsWith(`${name}=`));
+
+  return cookie ? decodeURIComponent(cookie.split("=")[1]) : null;
+};
